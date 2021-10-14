1e8c84fa7aaf5e2d48499c56a8a08f0ee319d5af
LinzichangDing <linzichang.ding@uqconnect.edu.au>
2021-10-14 19:51:48 +1000

    register update

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,3 +1,4 @@
+/** 
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { IonicAuthService } from '../ionic-auth.service';
@@ -43,6 +44,90 @@ export class RegisterPage implements OnInit {
   constructor(
     private router: Router,
     private ionicAuthService: IonicAuthService,
+    private fb: FormBuilder,
+  ) { }
+
+  ngOnInit() {
+    this.userForm = this.fb.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(6),
+        Validators.required
+      ])),
+    });
+  }
+
+  
+  signUp(value) {
+    this.ionicAuthService.createUser(value)
+      .then((response) => {
+        this.errorMsg = "";
+        this.successMsg = "New user created.";
+      }, error => {
+        this.errorMsg = error.message;
+        this.successMsg = "";
+      })
+
+  }
+
+
+  goToLogin() {
+    this.router.navigateByUrl('login');
+  }
+
+}
+
+*/
+
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+  user: any = {};
+  userForm: FormGroup;
+  successMsg: string = '';
+  errorMsg: string = '';
+  
+
+  error_msg = {
+    'email': [
+      { 
+        type: 'required', 
+        message: 'Provide email.' 
+      },
+      { 
+        type: 'pattern', 
+        message: 'Email is not valid.' 
+      }
+    ],
+    'password': [
+      { 
+        type: 'required', 
+        message: 'Password is required.' 
+      },
+      { 
+        type: 'minlength', 
+        message: 'Password length should be 6 characters long.' 
+      }
+    ]
+  };
+
+  constructor(
+    private auth: AngularFireAuth,
+    private router: Router,
+    private afDB: AngularFireDatabase,
     private fb: FormBuilder
   ) { }
 
@@ -59,15 +144,28 @@ export class RegisterPage implements OnInit {
     });
   }
 
-  signUp(value) {
-    this.ionicAuthService.createUser(value)
-      .then((response) => {
-        this.errorMsg = "";
-        this.successMsg = "New user created.";
-      }, error => {
-        this.errorMsg = error.message;
-        this.successMsg = "";
+  register() {
+
+    if (this.user.email && this.user.password) {
+      this.auth.createUserWithEmailAndPassword(this.user.email, this.user.password).then((r) => {
+        console.log(r);
+
+        this.afDB.object('users/' + r.user.uid).set({
+          name: this.user.name,
+          email: this.user.email,
+          gender: this.user.gender,
+          age: this.user.age,
+          weight: this.user.weight,
+          height: this.user.height,
+          createdAt: Date.now(),
+        }).then(() => {
+          this.router.navigateByUrl('/login');
+        });
+
+      }).catch(e => {
+        console.log(e);
       })
+    }
   }
 
   goToLogin() {
