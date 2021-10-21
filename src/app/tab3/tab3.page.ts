34af9a0889ddd00b96aac0a152cfed60c0908090
LinzichangDing <linzichang.ding@uqconnect.edu.au>
2021-10-21 15:43:49 +1000

    update tab3 tab1

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,61 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  man= "block"
+  woman= "none"
+  elderly= "none"
+  child = "none"
+  color = "rgb(255,235,148)"
+  constructor() {
+  }
+
+
+  showM() {
+    this.man = "block";
+    this.woman = "none";
+    this.elderly= "none";
+    this.child = "none";
+    document.getElementById("mcolor").style.backgroundColor="rgb(255,235,148)";
+    document.getElementById("wcolor").style.backgroundColor="rgb(255,235,148,0)";
+    document.getElementById("ecolor").style.backgroundColor="rgb(255,235,148,0)";
+    document.getElementById("ccolor").style.backgroundColor="rgb(255,235,148,0)";
+  }
+
+  showW() {
+    this.woman = "block";
+    this.man = "none";
+    this.elderly= "none";
+    this.child = "none";
+    document.getElementById("wcolor").style.backgroundColor="rgb(255,235,148)";
+    document.getElementById("mcolor").style.backgroundColor="rgb(255,235,148,0)";
+    document.getElementById("ecolor").style.backgroundColor="rgb(255,235,148,0)";
+    document.getElementById("ccolor").style.backgroundColor="rgb(255,235,148,0)";    
+  }
+
+  showE() {
+    this.elderly= "block";
+    this.woman = "none";
+    this.man = "none";
+    this.child = "none";
+    document.getElementById("ecolor").style.backgroundColor="rgb(255,235,148)";
+    document.getElementById("mcolor").style.backgroundColor="rgb(255,235,148,0)";
+    document.getElementById("wcolor").style.backgroundColor="rgb(255,235,148,0)";
+    document.getElementById("ccolor").style.backgroundColor="rgb(255,235,148,0)";
+  }
+
+  showC() {
+    this.child = "block";
+    this.man = "none";
+    this.woman = "none";
+    this.elderly= "none";
+    document.getElementById("ccolor").style.backgroundColor="rgb(255,235,148)";
+    document.getElementById("mcolor").style.backgroundColor="rgb(255,235,148,0)";
+    document.getElementById("wcolor").style.backgroundColor="rgb(255,235,148,0)";
+    document.getElementById("ecolor").style.backgroundColor="rgb(255,235,148,0)";
+
+
+  }
+
+
 
 }
