f7922fde0b24ab45e06d381802840e60e3d795c0
chenxuzhao97 <75033140+chenxuzhao97@users.noreply.github.com>
2021-10-22 21:43:16 +0800

    Update tab2.page.ts

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -16,7 +16,32 @@ export class Tab2Page {
     this.api.getFood(this.topic).subscribe(result=>{
       console.log(result);
       this.foodData=result['hints'];
+      var i;
+      console.log(this.foodData[0])
+      for (i in this.foodData) {
+        // console.log(111)
+        // console.log(this.foodData[i])
+        // console.log(this.foodData[i].food.nutrients.CHOCDF)
+        this.foodData[i].food.nutrients.CHOCDF = this.foodData[i].food.nutrients.CHOCDF.toFixed(2)
+        this.foodData[i].food.nutrients.ENERC_KCAL = this.foodData[i].food.nutrients.ENERC_KCAL.toFixed(2)
+        this.foodData[i].food.nutrients.FAT = this.foodData[i].food.nutrients.FAT.toFixed(2)
+        this.foodData[i].food.nutrients.FIBTG = this.foodData[i].food.nutrients.FIBTG.toFixed(2)
+        this.foodData[i].food.nutrients.PROCNT = this.foodData[i].food.nutrients.PROCNT.toFixed(2)
+        // item['food']['nutrients']['CHOCDF'] = Number(item['food']['nutrients']['HOCDF']).toFixed(2)
+        // item['food']['nutrients']['ENERC_KCAL'] = Number(item['food']['nutrients']['ENERC_KCAL']).toFixed(2)
+        // item['food']['nutrients']['FAT'] = Number(item['food']['nutrients']['FAT']).toFixed(2)
+        // item['food']['nutrients']['FIBTG'] = Number(item['food']['nutrients']['FIBTG']).toFixed(2)
+        // item['food']['nutrients']['PROCNT'] = Number(item['food']['nutrients']['PROCNT']).toFixed(2)
+
+        // item.food.nutrients.CHOCDF=0
+        // item['food']['nutrients']['ENERC_KCAL'] = 0
+        // item['food']['nutrients']['FAT'] = 0
+        // item['food']['nutrients']['FIBTG'] = 0
+        // item['food']['nutrients']['PROCNT'] = 0
+      }
     });
   }
 }
+
+
 
