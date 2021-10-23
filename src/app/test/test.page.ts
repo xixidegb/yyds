21a16539b3db69c2a0d718cb157a4f85d44eced2
xixidegb <>
2021-10-24 02:07:21 +1000

    add settings page
    settings page includes updating users' personal information and sign out function.

--- src/app/test/test.page.ts
+++ src/app/test/test.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage'
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { getAuth } from "firebase/auth";
+import { Router } from "@angular/router";
+import { IonicAuthService } from '../ionic-auth.service';
 
 @Component({
   selector: 'app-test',
@@ -7,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestPage implements OnInit {
 
-  constructor() { }
+  user: any = {};
+  getAuth = getAuth();
+  uid = this.getAuth.currentUser.uid
+
+  
+  constructor(
+    private auth: AngularFireAuth,
+    private afStorage: AngularFireStorage,
+    private afDB: AngularFireDatabase,
+    private router: Router,
+    private ionicAuthService: IonicAuthService
+  ) { }
 
   ngOnInit() {
+    this.afDB.object('users/' + this.uid).snapshotChanges().subscribe((usersnap: any) => {
+      this.user = { 'key': usersnap.key, ...usersnap.payload.val() };
+  })
   }
 
+
+  update() {
+    this.afDB.object('users/' + this.uid).update(this.user);
+    this.router.navigateByUrl('tabs/tabs/tab1');
+  }
+
+  signOut() {
+    this.ionicAuthService.signoutUser()
+      .then(res => {
+        this.router.navigateByUrl('welcome');
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
 }
